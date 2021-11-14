5cf536f8bf286db142ceccb2a33e41dbda7b352e
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-14 01:08:45 -0300

    Cambiar el nombre de los chunks

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,9 +2,18 @@ import { lazy, LazyExoticComponent } from 'react';
 
 type JSXComponent = () => JSX.Element;
 
-const LazyPage1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const LazyPage2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const LazyPage3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const LazyPage1 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1')
+);
+const LazyPage2 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2')
+);
+const LazyPage3 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3')
+);
 interface Route {
   path: string;
   component: LazyExoticComponent<JSXComponent> | JSXComponent; // () => JSX.Element
