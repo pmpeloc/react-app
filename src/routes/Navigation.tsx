0375c5ed5b248a99bc2d54401af6e9fd0b73d044
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-14 01:03:06 -0300

    LazyLoad y Suspense

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,30 +11,32 @@ import { routes } from './routes';
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className='main-layout'>
-        <nav>
-          <img src={logo} alt='Logo' />
-          <ul>
-            {routes.map(({ path, name }) => (
-              <li key={path}>
-                <NavLink to={path} activeClassName='nav-active' exact>
-                  {name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <Suspense fallback={<span>Loading...</span>}>
+      <Router>
+        <div className='main-layout'>
+          <nav>
+            <img src={logo} alt='Logo' />
+            <ul>
+              {routes.map(({ path, name }) => (
+                <li key={path}>
+                  <NavLink to={path} activeClassName='nav-active' exact>
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          {routes.map(({ path, component: Component }) => (
-            <Route key={path} path={path} render={() => <Component />} />
-          ))}
-          <Redirect to={routes[0].path} />
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            {routes.map(({ path, component: Component }) => (
+              <Route key={path} path={path} render={() => <Component />} />
+            ))}
+            <Redirect to={routes[0].path} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { lazy, LazyExoticComponent } from 'react';
 
 type JSXComponent = () => JSX.Element;
 
+const LazyPage1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
+const LazyPage2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
+const LazyPage3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
 interface Route {
   path: string;
-  component: JSXComponent;
+  component: LazyExoticComponent<JSXComponent> | JSXComponent; // () => JSX.Element
   name: string;
   children?: Route[];
 }
