36f36bfb7e18a449322879541afe084d3244771b
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-19 16:58:37 -0300

    Lazy Load Nested Listo

--- src/01-lazyload/layout/LazyLayout.tsx
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -1,8 +1,10 @@
+import { Navigation } from '../router/Navigation';
 export const LazyLayout = () => {
   return (
-    <>
+    <div>
       <h1>LazyLayout - Main Page</h1>
-    </>
+      <Navigation />
+    </div>
   );
 };
 
